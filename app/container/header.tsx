043c50d72f7ae6992a60fc74f4612b54b185176f
mdasif-me <cn.mdasif@gmail.com>
2026-03-04 15:28:46 +0600

    feat: Initialize Next.js project with Satoshi font, TailwindCSS, and foundational components.

---
+++ app/container/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className='bg-black text-white w-full text-center py-2.5 text-xs'>
+      Introducing The Full Arch Growth <span className='ordinal'>Formula</span>
+      <sup>TM</sup>, the dental marketing book for specialists. Claim your FREE
+      copy!
+    </div>
+  );
+}
---
+++ app/container/hero.tsx
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/hero.svg')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        height: '100vh',
+        width: '100%',
+      }}
+    >
+      Hero
+    </div>
+  );
+}
